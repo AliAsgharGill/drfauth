2ad1460e27d5a9e96d299f31fb68a640ed4449d4
AliAsgharGill <stylinalivlogs@gmail.com>
2024-09-27 19:57:26 +0500

    renderers added for errors

--- account/views.py
+++ account/views.py
@@ -3,21 +3,25 @@ from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from django.contrib.auth import authenticate
+from .renderers import UserRenderer
 class UserRegistrationView(APIView):
+    renderer_classes = (UserRenderer,)
     def post(self, request, format=None):
         serializer = UserRegistrationSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED) 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
 class UserLoginView(APIView):
-    def post(self, request,format=None):
+    renderer_classes = (UserRenderer,)
+    def post(self, request, format=None):
+
         serializer = UserLoginSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             email = serializer.data.get('email')
             password = serializer.data.get('password')
             user = authenticate(email=email, password=password)
-            if user is not None: 
+            if user is not None:
                 return Response({"message":"Login Success"}, status=status.HTTP_200_OK)
             return Response({"errors":{'non_field_errors':['Unable to log in with provided credentials']}}, status=status.HTTP_400_BAD_REQUEST)
