c2a5cdf133dabd8ffee39310244620fd2b3f569a
AliAsgharGill <stylinalivlogs@gmail.com>
2024-09-28 14:57:26 +0500

    change password working

--- account/serializers.py
+++ account/serializers.py
@@ -30,3 +30,22 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name']
+
+class UserChangePasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(max_length=255, style={'input_type': 'password'}, write_only=True)
+    password2 = serializers.CharField(max_length=255, style={'input_type': 'password'}, write_only=True)
+    class Meta:
+        fields = ['password', 'password2']
+        # extra_kwargs = {
+        #     'password': {'write_only': True}
+        # }
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        user = self.context.get('user')
+        if password != password2:
+            raise serializers.ValidationError("Passwords didn't match!")
+        user.set_password(password)
+        user.save()
+        return attrs
--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserProfileView, UserRegistrationView, UserLoginView
+from .views import UserProfileView, UserRegistrationView, UserLoginView, UserChangePasswordView
 urlpatterns = [
     path('register/', UserRegistrationView.as_view() , name='register'),
     path('login/', UserLoginView.as_view() , name='login'),
     path('profile/', UserProfileView.as_view() , name='profile'),
+    path('change-password/', UserChangePasswordView.as_view() , name='change-password'),
 ]
